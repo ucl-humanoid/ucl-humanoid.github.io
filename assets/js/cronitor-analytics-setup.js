4756ae4c7fdcce2d385fd04bb0d949a550db6865
Chengxu Zhou <zhouchengxu@gmail.com>
2026-09-25 15:20:44 +0100

    Publish: 2026-09-25T15:20:44+01:00

---
+++ assets/js/cronitor-analytics-setup.js
@@ -0,0 +1,6 @@
+window.cronitor =
+  window.cronitor ||
+  function () {
+    (window.cronitor.q = window.cronitor.q || []).push(arguments);
+  };
+cronitor("config", { clientKey: "090097000386ef8ca2c5ea849a698174" });
---
+++ assets/js/search-data.js
@@ -0,0 +1,59 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-people",
+          title: "people",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-join-us",
+          title: "join us",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/vacancies/";
+          },
+        },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
